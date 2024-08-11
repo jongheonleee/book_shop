f7f8a42dcbdc12dd48e70a1b082344f08f16da83
Jun <oowtl7425@gmail.com>
2024-08-12 01:58:58 +0900

    사용하지 않는 클래스 삭제 - TempBookDao - TempBookType - TempBookTest

--- src/main/java/com/fastcampus/ch4/dao/order/temp/TempBookDao.java
+++
@@ -1,7 +0,0 @@
-package com.fastcampus.ch4.dao.order.temp;
-
-import com.fastcampus.ch4.dto.order.temp.TempBookDto;
-
-public interface TempBookDao {
-    public TempBookDto selectByIsbn(String isbn);
-}

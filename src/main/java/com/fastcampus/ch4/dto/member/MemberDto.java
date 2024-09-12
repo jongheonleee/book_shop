f487ba6f91452299953767f218b61b38ea85b96b
haechangchoi <buraldongja@naver.com>
2024-09-12 15:42:59 +0900

    라스트

--- src/main/java/com/fastcampus/ch4/dto/member/MemberDto.java
+++ src/main/java/com/fastcampus/ch4/dto/member/MemberDto.java
@@ -212,14 +212,14 @@ public class MemberDto {
   }
 
 
-  public String getRole() {
-    return role;
-  }
-
-  public void setRole(String role) {
-    this.role = role;
-  }
-
-  private String role;
+//  public String getRole() {
+//    return role;
+//  }
+//
+//  public void setRole(String role) {
+//    this.role = role;
+//  }
+//
+//  private String role;
 
 }
--- src/main/java/com/fastcampus/ch4/service/member/MemberDetailService.java
+++ src/main/java/com/fastcampus/ch4/service/member/MemberDetailService.java
@@ -1,8 +1,8 @@
-package com.fastcampus.ch4.service.member;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface MemberDetailService {
-    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-}
+//package com.fastcampus.ch4.service.member;
+//
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//
+//public interface MemberDetailService {
+//    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+//}
